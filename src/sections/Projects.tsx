4ad71504931483195eeaa38053ad5b50839e9be0
gustiwdr <139354034+gustiwdr@users.noreply.github.com>
2025-07-09 09:30:05 +0700

    chore: change 'live site' to 'website'

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -173,7 +173,7 @@ export const ProjectsSection = () => {
 										{project.link && (
 											<a href={project.link}>
 												<button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2">
-													<span>Live Site</span>
+													<span>Website</span>
 													<ArrowUpRightIcon className="size-4" />
 												</button>
 											</a>
